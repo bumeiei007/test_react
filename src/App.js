a11570ad7bd1d4f42615be02a7e2667a24bf295e
bumeiei007 <bum0623121638@gmail.com>
2025-01-30 13:55:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://img.salehere.co.th/p/1200x0/2021/09/11/jhhxjok5rxn1.jpg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          00000
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
